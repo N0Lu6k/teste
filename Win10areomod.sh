d45c0cdbec91b03b7421a21674a2ba61f26067e6
N0Lu6k <154499206+N0Lu6k@users.noreply.github.com>
2024-04-16 15:29:08 +0800

    Update Win10areomod.sh

--- Win10areomod.sh
+++ Win10areomod.sh
@@ -1,20 +1,7 @@
 sudo apt-get update && apt-get install qemu -y
-sudo apt install qemu-utils -y
-sudo apt install qemu-system-x86-xen -y
-sudo apt install qemu-system-x86 -y
+sudo apt install qemu-kvm -y
 qemu-img create -f raw win.img 32G
 wget -O virtio-win.iso 'https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.215-1/virtio-win-0.1.215.iso'
 wget -O win.iso 'https://go.microsoft.com/fwlink/p/?LinkID=2195404&clcid=0x409&culture=en-us&country=US'
 clear
-sudo qemu-system-x86_64 \
-  -m 8G \
-  -cpu EPYC \
-  -boot order=d \
-  -drive file=win.iso,media=cdrom \
-  -drive file=win.img,format=raw,if=virtio \
-  -drive file=virtio-win.iso,media=cdrom \
-  -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
-  -device usb-tablet \
-  -vnc :0 \
-  -smp cores=2 \
-./ngrok tcp 5900
+sudo qemu-system-x86_64 -m 12G -smp 4 -cpu host -boot order=c -drive file=win.iso,media=cdrom -drive file=win.img,format=raw -device usb-ehci,id=usb,bus=pci.0,addr=0x4 -device usb-tablet -vnc :0 -smp cores=4 -device e1000,netdev=n0 -netdev user,id=n0 -vga qxl -accel kvm -bios virtio-win.iso
