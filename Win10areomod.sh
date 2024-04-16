b6068a77dacebb758de0761848226750119fe604
N0Lu6k <154499206+N0Lu6k@users.noreply.github.com>
2024-04-16 14:45:06 +0800

    Update Win10areomod.sh

--- Win10areomod.sh
+++ Win10areomod.sh
@@ -6,4 +6,4 @@ tar -xf ngrok.tgz
 sudo apt update
 sudo apt install qemu-kvm -y
 qemu-img create -f raw win.img 32G
-sudo qemu-system-x86_64 -m 12G -smp 4 -cpu host -boot order=c -drive file=win.iso,media=cdrom -drive file=win.img,format=raw -device usb-ehci,id=usb,bus=pci.0,addr=0x4 -device usb-tablet -vnc :0 -smp cores=4 -device e1000,netdev=n0 -netdev user,id=n0 -vga qxl -accel kvm -bios bios64.bin
+sudo qemu-system-x86_64 -m 12G -smp 4 -cpu host -boot order=c -drive file=win.iso,media=cdrom -drive file=win.img,format=raw -device usb-ehci,id=usb,bus=pci.0,addr=0x4 -device usb-tablet -vnc :0 -smp cores=4 -device e1000,netdev=n0 -netdev user,id=n0 -vga qxl -bios bios64.bin
