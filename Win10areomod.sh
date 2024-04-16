3a2365b567cee678e54842fac91e0926a1a45488
N0Lu6k <154499206+N0Lu6k@users.noreply.github.com>
2024-04-16 15:07:25 +0800

    Update Win10areomod.sh

--- Win10areomod.sh
+++ Win10areomod.sh
@@ -4,7 +4,7 @@ sudo apt install qemu-system-x86-xen -y
 sudo apt install qemu-system-x86 -y
 qemu-img create -f raw win.img 32G
 wget -O virtio-win.iso 'https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.215-1/virtio-win-0.1.215.iso'
-wget -O win.iso 'https://go.microsoft.com/fwlink/p/?LinkID=2195404&clcid=0x409&culture=en-us&country=US'
+wget -O win.iso "https://go.microsoft.com/fwlink/p/?LinkID=2195404&clcid=0x409&culture=en-us&country=US"
 clear
 sudo qemu-system-x86_64 \
   -m 8G \
@@ -16,4 +16,4 @@ sudo qemu-system-x86_64 \
   -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
   -device usb-tablet \
   -vnc :0 \
-  -smp cores=2 
+  -smp cores=2 \
